fb7e5c6f70e03ccd056fbd3343e0ee507ba2a385
krzysiu <krzysna@gmail.com>
2024-03-15 17:08:46 +0100

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# go_getter
+
+implementation of a "GoGetter" game from the Smart Games series
---
+++ android/app/src/main/kotlin/pl/edu/pg/GoGetter/go_getter/MainActivity.kt
@@ -0,0 +1,5 @@
+package pl.edu.pg.GoGetter.go_getter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
